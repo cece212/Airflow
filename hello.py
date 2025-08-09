ce014fa0efbc18777e8925f616dc0728d9a127da
cece212 <cecekrit@gmail.com>
2025-08-09 14:30:03 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 # 08/09/2025
 # A sample Python program to print a message 
 
-  print("Hello, World!")
+# Celina Krit 
+# 08/09/2025 
+# added new message 
+
+print("Hello, World!")
+
+print("Welcome to Github!")
